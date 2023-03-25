853cb23b07a9fb3f78456c4ceaabf0e62c14e7c5
pooetitu <pooetitu@gmail.com>
2023-03-25 05:21:37 +0100

    Change streamed chunks to fetched unitary chunks

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"github.com/abema/go-mp4"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
@@ -8,7 +10,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"io"
 	"log"
 	"net"
 	"net/http"
@@ -31,116 +32,156 @@ type MediaChunk struct {
 	Size       int64
 }
 
-type MediaDescription struct {
-	MetadataSize uint64
-	Chunks       []MediaChunk
+type MediaMetadata struct {
+	MetadataSize  uint64
+	VideoDuration float64
+	AudioTracks   []*pb.TrackLanguage
+	TextTracks    []*pb.TrackLanguage
 }
 
-func max(a, b float64) float64 {
-	if a > b {
-		return a
+func ReadMetadata(file *os.File) (*MediaMetadata, error) {
+	moov, err := mp4.ExtractBox(file, nil, mp4.BoxPath{mp4.BoxTypeMoov()})
+	if err != nil {
+		return nil, err
 	}
-	return b
-}
-
-func mapTracks(tracks mp4.Tracks) map[uint32]*mp4.Track {
-	m := make(map[uint32]*mp4.Track)
-	for _, track := range tracks {
-		m[track.TrackID] = track
+	mediaMetadata := MediaMetadata{
+		MetadataSize: moov[0].Size,
+		AudioTracks:  make([]*pb.TrackLanguage, 0),
+		TextTracks:   make([]*pb.TrackLanguage, 0),
 	}
-	return m
+	hdlrBoxes, err := mp4.ExtractBoxWithPayload(file, nil, mp4.BoxPath{mp4.BoxTypeMoov(), mp4.BoxTypeTrak(), mp4.BoxTypeMdia(), mp4.BoxTypeHdlr()})
+	mdhdBoxes, err := mp4.ExtractBoxWithPayload(file, nil, mp4.BoxPath{mp4.BoxTypeMoov(), mp4.BoxTypeTrak(), mp4.BoxTypeMdia(), mp4.BoxTypeMdhd()})
+	if err != nil {
+		return nil, err
+	}
+	for i := range hdlrBoxes {
+		hldr := hdlrBoxes[i].Payload.(*mp4.Hdlr)
+		mdhd := mdhdBoxes[i].Payload.(*mp4.Mdhd)
+		if hldr.HandlerType == [4]byte{'s', 'o', 'u', 'n'} {
+			mediaMetadata.AudioTracks = append(mediaMetadata.AudioTracks, &pb.TrackLanguage{Name: hldr.Name, Language: bytesToAsciiStr(mdhd.Language)})
+		} else if hldr.HandlerType == [4]byte{'s', 'b', 't', 'l'} {
+			mediaMetadata.TextTracks = append(mediaMetadata.TextTracks, &pb.TrackLanguage{Name: hldr.Name, Language: bytesToAsciiStr(mdhd.Language)})
+		}
+	}
+	return &mediaMetadata, nil
 }
 
-func ReadMediaDescription(file *os.File) (*MediaDescription, error) {
+func findVideoTrack(file *os.File) (*mp4.Track, error) {
+	hdlrBoxes, err := mp4.ExtractBoxWithPayload(file, nil, mp4.BoxPath{mp4.BoxTypeMoov(), mp4.BoxTypeTrak(), mp4.BoxTypeMdia(), mp4.BoxTypeHdlr()})
+	mdhdBoxes, err := mp4.ExtractBoxWithPayload(file, nil, mp4.BoxPath{mp4.BoxTypeMoov(), mp4.BoxTypeTrak(), mp4.BoxTypeMdia(), mp4.BoxTypeMdhd()})
+	tkhdBoxes, err := mp4.ExtractBoxWithPayload(file, nil, mp4.BoxPath{mp4.BoxTypeMoov(), mp4.BoxTypeTrak(), mp4.BoxTypeTkhd()})
+	if err != nil {
+		return nil, err
+	}
+	for i := range hdlrBoxes {
+		hldr := hdlrBoxes[i].Payload.(*mp4.Hdlr)
+		tkhd := tkhdBoxes[i].Payload.(*mp4.Tkhd)
+		mdhd := mdhdBoxes[i].Payload.(*mp4.Mdhd)
+		if hldr.HandlerType == [4]byte{'v', 'i', 'd', 'e'} {
+			return &mp4.Track{TrackID: tkhd.TrackID, Timescale: mdhd.Timescale}, nil
+		}
+	}
+	return nil, errors.New("not found")
+}
+
+func bytesToAsciiStr(bytes [3]byte) string {
+	for i, value := range bytes {
+		bytes[i] = value + 96
+	}
+	return string(bytes[:])
+}
+
+func ReadMediaDescription(file *os.File) ([]*MediaChunk, error) {
 	info, err := mp4.Probe(bufseekio.NewReadSeeker(file, 1024, 4))
 	if err != nil {
 		return nil, err
 	}
-	mediaDescription := MediaDescription{
-		MetadataSize: info.Segments[0].MoofOffset,
-		Chunks:       make([]MediaChunk, len(info.Segments)),
+	chunks := make([]*MediaChunk, 0)
+
+	videoTrack, err := findVideoTrack(file)
+	if err != nil {
+		return nil, err
 	}
-	tracks := mapTracks(info.Tracks)
 	var timer float64
 	for i := 0; i < len(info.Segments); i++ {
 		segment := info.Segments[i]
-		duration := float64(segment.Duration) / float64(tracks[segment.TrackID].Timescale)
-		var endTime float64
-		if tracks[segment.TrackID].MP4A != nil {
-			endTime = timer + duration
-		} else {
-			endTime = 0
+		if videoTrack.TrackID != segment.TrackID {
+			continue
 		}
+		fmt.Printf("%d\n", segment.TrackID)
+		duration := float64(segment.Duration) / float64(videoTrack.Timescale)
+		endTime := timer + duration
 		chunk := MediaChunk{
 			ByteOffset: int64(segment.MoofOffset),
 			StartTime:  timer,
 			EndTime:    endTime,
 		}
 		timer = endTime
-		mediaDescription.Chunks[i] = chunk
-		if i > 0 {
-			mediaDescription.Chunks[i-1].Size = int64(segment.MoofOffset) - mediaDescription.Chunks[i-1].ByteOffset
+		chunks = append(chunks, &chunk)
+		if len(chunks) > 0 {
+			chunks[len(chunks)-1].Size = int64(segment.MoofOffset) - chunks[len(chunks)-1].ByteOffset
 		}
 	}
 	fi, _ := file.Stat()
-	mediaDescription.Chunks[len(info.Segments)-1].Size = fi.Size()
-	return &mediaDescription, nil
+	chunks[len(chunks)-1].Size = fi.Size()
+	fmt.Printf("%v\n", chunks[len(chunks)-1])
+	return chunks, nil
 }
 
-func (m *MediaDescription) SeekChunk(timeCode float64) (int, error) {
-	for index, chunk := range m.Chunks {
-		if timeCode >= chunk.StartTime && timeCode <= chunk.EndTime {
+func SeekChunk(chunks []*MediaChunk, timeCode float64) (int, error) {
+	for index, chunk := range chunks {
+		if timeCode >= chunk.StartTime && timeCode < chunk.EndTime {
 			return index, nil
 		}
 	}
 	return 0, status.Errorf(codes.NotFound, "Impossible de trouver un segment pour le time code %f", timeCode)
 }
 
-func (s *server) GetVideoStream(req *pb.VideoRequest, stream pb.VideoService_GetVideoStreamServer) error {
+func (s *server) GetVideoMetadata(_ context.Context, req *pb.VideoMetadataRequest) (*pb.VideoMetadata, error) {
 	file, err := os.Open(fmt.Sprintf("./out/%d.mp4", req.VideoId))
 	if err != nil {
-		return status.Errorf(codes.Internal, "Erreur de lecture du media: %v", err)
+		return nil, status.Errorf(codes.Internal, "Erreur de lecture du media: %v", err)
 	}
 	defer file.Close()
-	mediaDescription, err := ReadMediaDescription(file)
+	mediaMetadata, err := ReadMetadata(file)
 
-	if _, err = file.Seek(0, 0); err != nil {
-		return err
+	buf := make([]byte, mediaMetadata.MetadataSize)
+	if _, err := file.Seek(0, int(mediaMetadata.MetadataSize)); err != nil {
+		return nil, err
 	}
-	// Send metadata
-	buf := make([]byte, mediaDescription.MetadataSize)
 	n, err := file.Read(buf)
 	if err != nil {
-		return status.Errorf(codes.Internal, "Erreur de lecture du fichier de sortie: %v", err)
-	}
-	i, err := mediaDescription.SeekChunk(max(0, req.Seek-60))
-	if err != nil {
-		return err
-	}
-	if err := stream.Send(&pb.VideoResponse{Metadata: buf[:n]}); err != nil {
-		return status.Errorf(codes.Internal, "Erreur d'envoi des données de la vidéo: %v", err)
-	}
-	// Send chunks
-	for ; i < len(mediaDescription.Chunks); i++ {
-		chunk := mediaDescription.Chunks[i]
-		buf := make([]byte, chunk.Size)
-		if _, err := file.Seek(chunk.ByteOffset, 0); err != nil {
-			return err
-		}
-		n, err := file.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return status.Errorf(codes.Internal, "Erreur de lecture du fichier de sortie: %v", err)
-		}
-		chunkRes := &pb.VideoResponse{Data: buf[:n], StartTime: &chunk.StartTime, EndTime: &chunk.EndTime}
-		if err := stream.Send(chunkRes); err != nil {
-			return status.Errorf(codes.Internal, "Erreur d'envoi des données de la vidéo: %v", err)
-		}
+		return nil, status.Errorf(codes.Internal, "Erreur de lecture du fichier de sortie: %v", err)
 	}
 
-	return nil
+	return &pb.VideoMetadata{
+		Metadata:            buf[:n],
+		VideoDuration:       mediaMetadata.VideoDuration,
+		TextTrackLanguages:  mediaMetadata.TextTracks,
+		AudioTrackLanguages: mediaMetadata.AudioTracks,
+	}, nil
+}
+
+func (s *server) GetVideoChunk(_ context.Context, req *pb.VideoChunkRequest) (*pb.VideoChunk, error) {
+	file, err := os.Open(fmt.Sprintf("./out/%d.mp4", req.VideoId))
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Erreur de lecture du media: %v", err)
+	}
+	defer file.Close()
+	chunks, err := ReadMediaDescription(file)
+
+	chunkIndex, err := SeekChunk(chunks, req.Seek)
+	chunk := chunks[chunkIndex]
+	buf := make([]byte, chunk.Size)
+	if _, err := file.Seek(chunk.ByteOffset, 0); err != nil {
+		return nil, err
+	}
+	n, err := file.Read(buf)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Erreur de lecture du fichier de sortie: %v", err)
+	}
+
+	return &pb.VideoChunk{Data: buf[:n], StartTime: chunk.StartTime, EndTime: chunk.EndTime}, nil
 }
 
 func main() {
